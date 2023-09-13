da4c43303d5cd8878e298c905761593e9648b254
nathan.sineiro <nathan.sineiro@devnology.com.br>
2023-09-13 10:27:15 -0300

    feat: make the connection to the database

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const connection = require("./database/database");
+//Database
+const modelEmplyee = require("./database/employee");
+
+connection.authenticate().then(() => {
+    console.log("Conexão feita com o databas");
+}).catch((msg) => {
+    console.log(msg);
+})
 
 app.set('view engine', 'ejs');
 
